e6d0216615f7afa08f4d98abc7829509e68d342f
meidanzhu <meidan.zhu@gmail.com>
2020-05-25 21:28:18 -0400

    initial setup

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,12 @@
+#file to require all relative files and gems
+
 require "disney/version"
+require 'pry'
+require 'json'
 
 module Disney
   class Error < StandardError; end
   # Your code goes here...
 end
+
+
